ee573100b4e66529db4448131caf45923344d692
Helmoz <helmo.designer1@gmail.com>
2021-03-07 11:14:16 +0300

    fix dependentUpon after rename

--- Modules/AbdtPractice.Shop/Features/MyOrders/PayOrderCommandHandler.cs
+++ Modules/AbdtPractice.Shop/Features/MyOrders/PayOrderCommandHandler.cs
@@ -1,13 +1,12 @@
 ﻿using AbdtPractice.Core.Base;
 using AbdtPractice.Core.Entities;
 using Force.Ccc;
-using JetBrains.Annotations;
 
 namespace AbdtPractice.Shop.Features.MyOrders
 {
     public class PayOrderCommandHandler : ChangeOrderStateHandlerBase<PayOrder, Order.New, Order.Paid>
     {
-        public PayOrderCommandHandler([NotNull] IUnitOfWork unitOfWork) : base(unitOfWork) { }
+        public PayOrderCommandHandler(IUnitOfWork unitOfWork) : base(unitOfWork) { }
 
         protected override Order.Paid ChangeState(ChangeOrderStateContext<PayOrder, Order.New> input)
         {
